073421ed9e945334c164210ef41a314040fd422f
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-04-21 03:13:28 +0000

    user will now be routed to clipboard index page after logging in instead of home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    clipboards_path
+  end
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:general_manager_name, :logo, :organization_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:general_manager_name, :logo, :organization_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:general_manager_name, :organization_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:general_manager_name, :organization_name])
   end
 
 end
