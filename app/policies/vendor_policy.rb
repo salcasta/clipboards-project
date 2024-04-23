3165fb147fa16b436166ce532cba961498913a5c
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-04-23 01:06:08 +0000

    vendors routes are protected by pundit

---
+++ app/policies/vendor_policy.rb
@@ -0,0 +1,27 @@
+class VendorPolicy < ApplicationPolicy
+
+  def show?
+    user.id == record.user_id
+  end
+
+  def create?
+    user.id == record.user_id
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    user.id == record.user_id
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user.id == record.user_id
+  end
+
+end
