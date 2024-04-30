a8be76520edd94c48b0bf3c0984064a14d4e8a15
Salvador Castaneda <57605337+salcasta@users.noreply.github.com>
2024-04-30 02:04:45 +0000

    add ransack, add pagination, add button hover effect

--- app/controllers/holiday_controller.rb
+++ app/controllers/holiday_controller.rb
@@ -1,7 +1,7 @@
 class HolidayController < ApplicationController
 
   def index
-    @inventorysheets = current_user.inventorysheets.where(is_holiday: true).order(date: :desc)
+    @inventorysheets = current_user.inventorysheets.where(is_holiday: true).order(date: :desc).page(params[:items_ordered_page]).per(10)
   end
 
 end
